bc7287b7e8e2d255c027276ec77602bb82dd34fb
emperorsixpacks <andrewdvd10@gmail.com>
2025-03-27 17:56:29 +0100

    feat: Replace os.path.dirname with recursive get_base_dir function

--- sliderblend/pkg/utils/app_utils.py
+++ sliderblend/pkg/utils/app_utils.py
@@ -2,7 +2,15 @@ import os
 
 
 def return_base_dir():
-    return os.path.dirname(os.path.dirname(__file__))
+    return get_dir_at_level(3)
+
+
+def get_dir_at_level(level=1, file: str = __file__):
+    current_path = os.path.dirname(file)
+
+    if level == 0:
+        return os.path.dirname(file)
+    return get_dir_at_level(level - 1, current_path)
 
 
 def exists(path: str) -> bool:
@@ -12,7 +20,9 @@ def exists(path: str) -> bool:
 class Prompt:
     def __init__(self, prompt_name: str, /, **kwargs: str) -> None:
         self.prompt_name = prompt_name
-        self.prompt_path = os.path.join(BASE_DIR, f"prompts/{self.prompt_name}.txt")
+        self.prompt_path = os.path.join(
+            return_base_dir(), f"prompts/{self.prompt_name}.txt"
+        )
         self.prompt_params = kwargs
 
     def read(self) -> str:
