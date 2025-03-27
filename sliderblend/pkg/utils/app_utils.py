3acd9c15921964f038d0a1e582c3bab92c95dc18
emperorsixpacks <andrewdvd10@gmail.com>
2025-03-27 17:59:26 +0100

    fix

--- sliderblend/pkg/utils/app_utils.py
+++ sliderblend/pkg/utils/app_utils.py
@@ -1,5 +1,7 @@
 import os
 
+from fitz import r
+
 
 def return_base_dir():
     return get_dir_at_level(3)
@@ -7,7 +9,8 @@ def return_base_dir():
 
 def get_dir_at_level(level=1, file: str = __file__):
     current_path = os.path.dirname(file)
-
+    if level < 0:
+        raise ValueError("Level cannot be less than 0")
     if level == 0:
         return os.path.dirname(file)
     return get_dir_at_level(level - 1, current_path)
