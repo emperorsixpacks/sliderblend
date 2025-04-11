6ee27681efece4d9018ac7eed33448b6138f98ad
emperorsixpacks <andrewdvd10@gmail.com>
2025-04-11 18:25:26 +0100

    feat: redis session storage

--- sliderblend/web/routers/authRouter.py
+++ sliderblend/web/routers/authRouter.py
@@ -8,11 +8,13 @@ from fastapi.responses import JSONResponse
 from sliderblend.internal import UserModel
 from sliderblend.pkg import get_session
 from sliderblend.pkg.types import TelegramInitData
-from sliderblend.pkg.utils import verify_tg_init_data
+from sliderblend.pkg.utils import generate_session_key, verify_tg_init_data
+from sliderblend.web.schema import SessionData, UserCache
 
 if TYPE_CHECKING:
     from sqlmodel import Session
 
+    from sliderblend.internal import RedisClient
     from sliderblend.pkg import TelegramSettings
 
 
@@ -20,12 +22,15 @@ CALLBACK_PATH = "/callback"
 
 
 class AuthRouter:
-    def __init__(self, telegram_settings: TelegramSettings) -> None:
+    def __init__(
+        self, telegram_settings: TelegramSettings, redis_client: RedisClient
+    ) -> None:
         self.telegram_settings = telegram_settings
+        self.redis_client = redis_client
 
     def get_router(self):
         router = APIRouter(prefix="/auth")
-        router.add_api_route(CALLBACK_PATH, self.callback_url)
+        router.add_api_route(CALLBACK_PATH, self.callback_url, methods=["POST"])
         return router
 
     def callback_url(self, data: dict, session: Session = Depends(get_session)):
@@ -39,19 +44,26 @@ class AuthRouter:
                 content=err.message, status_code=status.HTTP_403_FORBIDDEN
             )
         user_data = init_data.user
-        _, err = UserModel.get(
+        user, err = UserModel.get(
             field="telegram_user_id",
             value=str(user_data.telegram_user_id),
             session=session,
         )
         if err:
-            new_user = UserModel(**user_data.model_dump())
-            err = new_user.create_user(session)
-            if err:
+            user = UserModel(**user_data.model_dump())
+            if err := user.create_user(session):
                 print(err.message)
                 return JSONResponse(
                     content=err.message, status_code=status.HTTP_403_FORBIDDEN
                 )
         session.commit()
-
-        return {"ok": True}
+        session_key = generate_session_key(user)
+        _, err = self.redis_client.create(
+            f"user:{session_key}", UserCache(**user.model_dump())
+        )
+        if err:
+            print(err.message)
+            return JSONResponse(
+                content=err.message, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        return {"session": session_key}
