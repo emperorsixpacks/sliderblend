689fa07380bde90b49f84dcda4ee43d65f1d6b9c
emperorsixpacks <andrewdvd10@gmail.com>
2025-05-05 00:24:17 +0100

    feat: removed db session from clients

--- sliderblend/internal/main.py
+++ sliderblend/internal/main.py
@@ -1,33 +1,50 @@
 from cohere import AsyncClientV2
 
-from sliderblend.internal import IBMStorage, RedisClient, RedisJob
+from sliderblend.internal.redis import RedisJob, RedisClient
+from sliderblend.internal.ibm_storage import IBMStorage
 from sliderblend.pkg import (
     AgentSettings,
     CohereSettings,
     IBMSettings,
     RedisSettings,
-    get_session,
+    get_logger,
 )
+
+logger = get_logger("init-clients")
 
 
 def init_clients() -> AgentSettings:
+    logger.info("Setting up client settigns")
+
     ibm_settings = IBMSettings()
-    redis_sttings = RedisSettings()
+    logger.debug(f"IBMSettings loaded: {ibm_settings}")
+
+    redis_settings = RedisSettings()
+    logger.debug(f"RedisSettings loaded: {redis_settings}")
+
     cohere_settings = CohereSettings()
+    logger.debug(f"CohereSettings loaded: {cohere_settings}")
 
     ibm_storage_repo = IBMStorage(ibm_settings)
-    redis_client = RedisClient(redis_sttings)
+    logger.info("IBM Storage client initialized.")
+
+    redis_client = RedisClient(redis_settings)
+    logger.info("Redis client initialized.")
+
     redis_job = RedisJob(redis_client)
+    logger.info("Redis job client initialized.")
 
     cohere_client = AsyncClientV2(cohere_settings.cohere_api_key)
+    logger.info("Cohere client initialized.")
+
+    logger.info("Database session created.")
 
     agent_settings = AgentSettings(
         REDIS_JOB_CLIENT=redis_job,
         REDIS_CLIENT=redis_client,
         COHERE_CLIENT=cohere_client,
-        DB_SESSION=get_session(),
         IBM_CLIENT=ibm_storage_repo,
     )
-    return agent_settings
 
-# diff between init_settings() -> T and init_settings() -> Type(T)
+    logger.info("Agent settings assembled and returned.")
+    return agent_settings
